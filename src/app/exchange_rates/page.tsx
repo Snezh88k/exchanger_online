1d8872450504d5a967dd8db0bc1adca14f13eaf1
Snezh88k <snezh88k@gmail.com>
2023-06-26 15:53:40 +0300

    add redirect

--- src/app/exchange_rates/page.tsx
+++ src/app/exchange_rates/page.tsx
@@ -1,34 +1,6 @@
-"use client";
-import { fetchRates } from "@/store/slice/rateSlice";
-import { AppDispatch } from "@/store/store";
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { redirect } from "next/navigation";
 import styles from "./page.module.scss";
-import Select from "../../../components/select/Select";
-
-interface State {
-  rates: {
-    rates: [];
-  };
-}
 
 export default function page() {
-  const [activeСurrency, setActiveСurrency] = useState("USD");
-  const dispatch = useDispatch<AppDispatch>();
-  const store = useSelector((state: State) => state.rates.rates);
-
-  useEffect(() => {
-    if (store.length === 0) {
-      dispatch(fetchRates("USD"));
-    }
-  }, []);
-
-  const changeCurrency = (e: any) => {
-    dispatch(fetchRates(e.target.value));
-    setActiveСurrency(e.target.value);
-  };
-
-  const currencies = ["USD", "RUB", "EUR", "BTC"];
-
-  return <div>МЕЙН ПЕЙДЖ</div>;
+  redirect("/exchange_rates/USD");
 }
